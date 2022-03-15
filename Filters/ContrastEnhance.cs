ee2278bf716f1246a6df6a9e7f9efe4d98a76696
wolftender <etheryt@protonmail.com>
2022-03-15 15:27:57 +0100

    implement functional filters

--- Filters/ContrastEnhance.cs
+++ Filters/ContrastEnhance.cs
@@ -9,8 +9,27 @@ namespace cg_proj_1.Filters {
 	class ContrastEnhance : IImageFilter {
 		public string Name => "Contrast Enhance";
 
+		private float slope;
+
+		public float Slope {
+			get { return slope; }
+			set { slope = value; }
+		}
+
+		public ContrastEnhance () {
+			slope = 1.25f;
+		}
+
 		public byte [] apply (byte [] bitmap, int width, int height, int stride) {
-			throw new NotImplementedException ();
+			int intercept = (int) ((1 - slope) * 127);
+			int filtered;
+
+			for (int i = 0; i < bitmap.Length; ++i) {
+				filtered = (int) Math.Round (slope * bitmap [i]) + intercept;
+				bitmap [i] = (byte) Math.Min (255, Math.Max (0, filtered));
+			}
+
+			return bitmap;
 		}
 	}
 }
--- Filters/GammaCorrection.cs
+++ Filters/GammaCorrection.cs
@@ -9,8 +9,26 @@ namespace cg_proj_1.Filters {
 	class GammaCorrection : IImageFilter {
 		public string Name => "Gamma Correct.";
 
+		private float gamma;
+
+		public float Gamma {
+			get { return gamma; }
+			set { gamma = value; }
+		}
+
+		public GammaCorrection () {
+			gamma = 0.75f;
+		}
+
 		public byte [] apply (byte [] bitmap, int width, int height, int stride) {
-			throw new NotImplementedException ();
+			int filtered;
+
+			for (int i = 0; i < bitmap.Length; ++i) {
+				filtered = (int) Math.Round (Math.Pow (bitmap [i] / 255.0f, 1.0f / gamma) * 255);
+				bitmap [i] = (byte) Math.Min (255, Math.Max (0, filtered));
+			}
+
+			return bitmap;
 		}
 	}
 }
