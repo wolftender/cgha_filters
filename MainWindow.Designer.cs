b9a21af453611d12b4708af831f6d63fe3ed2547
wolftender <etheryt@protonmail.com>
2022-03-29 18:36:58 +0200

    fix filter list on lower dpi

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -62,21 +62,25 @@ namespace cg_proj_1 {
 			// 
 			// mainStatusStrip
 			// 
-			this.mainStatusStrip.Location = new System.Drawing.Point(0, 749);
+			this.mainStatusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.mainStatusStrip.Location = new System.Drawing.Point(0, 1006);
 			this.mainStatusStrip.Name = "mainStatusStrip";
-			this.mainStatusStrip.Size = new System.Drawing.Size(1166, 22);
+			this.mainStatusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+			this.mainStatusStrip.Size = new System.Drawing.Size(1333, 22);
 			this.mainStatusStrip.TabIndex = 0;
 			this.mainStatusStrip.Text = "statusStrip1";
 			// 
 			// menuStrip1
 			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
 			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
             this.editToolStripMenuItem,
             this.aboutToolStripMenuItem});
 			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
 			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(1166, 24);
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
+			this.menuStrip1.Size = new System.Drawing.Size(1333, 30);
 			this.menuStrip1.TabIndex = 1;
 			this.menuStrip1.Text = "menuStrip1";
 			// 
@@ -87,27 +91,27 @@ namespace cg_proj_1 {
             this.saveImageToolStripMenuItem,
             this.exitToolStripMenuItem});
 			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
 			this.fileToolStripMenuItem.Text = "File";
 			// 
 			// loadImageToolStripMenuItem
 			// 
 			this.loadImageToolStripMenuItem.Name = "loadImageToolStripMenuItem";
-			this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.loadImageToolStripMenuItem.Size = new System.Drawing.Size(171, 26);
 			this.loadImageToolStripMenuItem.Text = "Load Image";
 			this.loadImageToolStripMenuItem.Click += new System.EventHandler(this.loadImageToolStripMenuItem_Click);
 			// 
 			// saveImageToolStripMenuItem
 			// 
 			this.saveImageToolStripMenuItem.Name = "saveImageToolStripMenuItem";
-			this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.saveImageToolStripMenuItem.Size = new System.Drawing.Size(171, 26);
 			this.saveImageToolStripMenuItem.Text = "Save Image";
 			this.saveImageToolStripMenuItem.Click += new System.EventHandler(this.saveImageToolStripMenuItem_Click);
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-			this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(171, 26);
 			this.exitToolStripMenuItem.Text = "Exit";
 			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
 			// 
@@ -118,26 +122,26 @@ namespace cg_proj_1 {
             this.loadFilterToolStripMenuItem,
             this.clearAllFiltersToolStripMenuItem});
 			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-			this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+			this.editToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
 			this.editToolStripMenuItem.Text = "Edit";
 			// 
 			// addFilterToolStripMenuItem
 			// 
 			this.addFilterToolStripMenuItem.Name = "addFilterToolStripMenuItem";
-			this.addFilterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.addFilterToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
 			this.addFilterToolStripMenuItem.Text = "Add Filter";
 			this.addFilterToolStripMenuItem.Click += new System.EventHandler(this.addFilterToolStripMenuItem_Click);
 			// 
 			// loadFilterToolStripMenuItem
 			// 
 			this.loadFilterToolStripMenuItem.Name = "loadFilterToolStripMenuItem";
-			this.loadFilterToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.loadFilterToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
 			this.loadFilterToolStripMenuItem.Text = "Load Filter";
 			// 
 			// clearAllFiltersToolStripMenuItem
 			// 
 			this.clearAllFiltersToolStripMenuItem.Name = "clearAllFiltersToolStripMenuItem";
-			this.clearAllFiltersToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.clearAllFiltersToolStripMenuItem.Size = new System.Drawing.Size(191, 26);
 			this.clearAllFiltersToolStripMenuItem.Text = "Clear All Filters";
 			// 
 			// aboutToolStripMenuItem
@@ -145,13 +149,13 @@ namespace cg_proj_1 {
 			this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.aboutFilterEditorToolStripMenuItem});
 			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
 			this.aboutToolStripMenuItem.Text = "About";
 			// 
 			// aboutFilterEditorToolStripMenuItem
 			// 
 			this.aboutFilterEditorToolStripMenuItem.Name = "aboutFilterEditorToolStripMenuItem";
-			this.aboutFilterEditorToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+			this.aboutFilterEditorToolStripMenuItem.Size = new System.Drawing.Size(223, 26);
 			this.aboutFilterEditorToolStripMenuItem.Text = "About Filter Editor...";
 			// 
 			// splitContainer1
@@ -159,7 +163,8 @@ namespace cg_proj_1 {
 			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
 			this.splitContainer1.IsSplitterFixed = true;
-			this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+			this.splitContainer1.Location = new System.Drawing.Point(0, 30);
+			this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.splitContainer1.Name = "splitContainer1";
 			// 
 			// splitContainer1.Panel1
@@ -169,8 +174,9 @@ namespace cg_proj_1 {
 			// splitContainer1.Panel2
 			// 
 			this.splitContainer1.Panel2.Controls.Add(this.tableLayoutPanel1);
-			this.splitContainer1.Size = new System.Drawing.Size(1166, 725);
-			this.splitContainer1.SplitterDistance = 920;
+			this.splitContainer1.Size = new System.Drawing.Size(1333, 976);
+			this.splitContainer1.SplitterDistance = 1084;
+			this.splitContainer1.SplitterWidth = 5;
 			this.splitContainer1.TabIndex = 2;
 			// 
 			// tableLayoutPanel2
@@ -182,10 +188,11 @@ namespace cg_proj_1 {
 			this.tableLayoutPanel2.Controls.Add(this.pictureBoxRaw, 0, 0);
 			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
 			this.tableLayoutPanel2.RowCount = 1;
 			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel2.Size = new System.Drawing.Size(920, 725);
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(1084, 976);
 			this.tableLayoutPanel2.TabIndex = 0;
 			// 
 			// pictureBoxFiltered
@@ -193,9 +200,10 @@ namespace cg_proj_1 {
 			this.pictureBoxFiltered.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.pictureBoxFiltered.Location = new System.Drawing.Point(463, 3);
+			this.pictureBoxFiltered.Location = new System.Drawing.Point(545, 4);
+			this.pictureBoxFiltered.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.pictureBoxFiltered.Name = "pictureBoxFiltered";
-			this.pictureBoxFiltered.Size = new System.Drawing.Size(454, 719);
+			this.pictureBoxFiltered.Size = new System.Drawing.Size(536, 968);
 			this.pictureBoxFiltered.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
 			this.pictureBoxFiltered.TabIndex = 0;
 			this.pictureBoxFiltered.TabStop = false;
@@ -205,27 +213,27 @@ namespace cg_proj_1 {
 			this.pictureBoxRaw.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-			this.pictureBoxRaw.Location = new System.Drawing.Point(3, 3);
+			this.pictureBoxRaw.Location = new System.Drawing.Point(3, 4);
+			this.pictureBoxRaw.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.pictureBoxRaw.Name = "pictureBoxRaw";
-			this.pictureBoxRaw.Size = new System.Drawing.Size(454, 719);
+			this.pictureBoxRaw.Size = new System.Drawing.Size(536, 968);
 			this.pictureBoxRaw.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
 			this.pictureBoxRaw.TabIndex = 1;
 			this.pictureBoxRaw.TabStop = false;
 			// 
 			// tableLayoutPanel1
 			// 
-			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.tableLayoutPanel1.ColumnCount = 1;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
 			this.tableLayoutPanel1.Controls.Add(this.filtersGroupBox, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-			this.tableLayoutPanel1.RowCount = 1;
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 714F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(242, 725);
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(244, 976);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// filtersGroupBox
@@ -235,19 +243,24 @@ namespace cg_proj_1 {
 			this.filtersGroupBox.Controls.Add(this.buttonDeleteFilter);
 			this.filtersGroupBox.Controls.Add(this.buttonEditFilter);
 			this.filtersGroupBox.Controls.Add(this.buttonAddFilter);
-			this.filtersGroupBox.Location = new System.Drawing.Point(3, 3);
+			this.filtersGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.filtersGroupBox.Location = new System.Drawing.Point(3, 4);
+			this.filtersGroupBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.filtersGroupBox.Name = "filtersGroupBox";
-			this.filtersGroupBox.Size = new System.Drawing.Size(236, 285);
+			this.filtersGroupBox.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.filtersGroupBox.Size = new System.Drawing.Size(238, 344);
 			this.filtersGroupBox.TabIndex = 0;
 			this.filtersGroupBox.TabStop = false;
 			this.filtersGroupBox.Text = "Filters";
 			// 
 			// buttonCustom
 			// 
+			this.buttonCustom.Dock = System.Windows.Forms.DockStyle.Bottom;
 			this.buttonCustom.Enabled = false;
-			this.buttonCustom.Location = new System.Drawing.Point(7, 255);
+			this.buttonCustom.Location = new System.Drawing.Point(3, 216);
+			this.buttonCustom.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.buttonCustom.Name = "buttonCustom";
-			this.buttonCustom.Size = new System.Drawing.Size(220, 23);
+			this.buttonCustom.Size = new System.Drawing.Size(232, 31);
 			this.buttonCustom.TabIndex = 5;
 			this.buttonCustom.Text = "Make Custom";
 			this.buttonCustom.UseVisualStyleBackColor = true;
@@ -257,11 +270,13 @@ namespace cg_proj_1 {
 			// 
 			this.activeFiltersList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1});
+			this.activeFiltersList.Dock = System.Windows.Forms.DockStyle.Top;
 			this.activeFiltersList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
 			this.activeFiltersList.HideSelection = false;
-			this.activeFiltersList.Location = new System.Drawing.Point(6, 22);
+			this.activeFiltersList.Location = new System.Drawing.Point(3, 24);
+			this.activeFiltersList.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.activeFiltersList.Name = "activeFiltersList";
-			this.activeFiltersList.Size = new System.Drawing.Size(224, 139);
+			this.activeFiltersList.Size = new System.Drawing.Size(232, 184);
 			this.activeFiltersList.TabIndex = 4;
 			this.activeFiltersList.UseCompatibleStateImageBehavior = false;
 			this.activeFiltersList.View = System.Windows.Forms.View.Details;
@@ -274,10 +289,12 @@ namespace cg_proj_1 {
 			// 
 			// buttonDeleteFilter
 			// 
+			this.buttonDeleteFilter.Dock = System.Windows.Forms.DockStyle.Bottom;
 			this.buttonDeleteFilter.Enabled = false;
-			this.buttonDeleteFilter.Location = new System.Drawing.Point(6, 225);
+			this.buttonDeleteFilter.Location = new System.Drawing.Point(3, 247);
+			this.buttonDeleteFilter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.buttonDeleteFilter.Name = "buttonDeleteFilter";
-			this.buttonDeleteFilter.Size = new System.Drawing.Size(224, 23);
+			this.buttonDeleteFilter.Size = new System.Drawing.Size(232, 31);
 			this.buttonDeleteFilter.TabIndex = 3;
 			this.buttonDeleteFilter.Text = "Delete Filter";
 			this.buttonDeleteFilter.UseVisualStyleBackColor = true;
@@ -285,10 +302,12 @@ namespace cg_proj_1 {
 			// 
 			// buttonEditFilter
 			// 
+			this.buttonEditFilter.Dock = System.Windows.Forms.DockStyle.Bottom;
 			this.buttonEditFilter.Enabled = false;
-			this.buttonEditFilter.Location = new System.Drawing.Point(6, 196);
+			this.buttonEditFilter.Location = new System.Drawing.Point(3, 278);
+			this.buttonEditFilter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.buttonEditFilter.Name = "buttonEditFilter";
-			this.buttonEditFilter.Size = new System.Drawing.Size(224, 23);
+			this.buttonEditFilter.Size = new System.Drawing.Size(232, 31);
 			this.buttonEditFilter.TabIndex = 2;
 			this.buttonEditFilter.Text = "Edit Filter";
 			this.buttonEditFilter.UseVisualStyleBackColor = true;
@@ -296,9 +315,11 @@ namespace cg_proj_1 {
 			// 
 			// buttonAddFilter
 			// 
-			this.buttonAddFilter.Location = new System.Drawing.Point(6, 167);
+			this.buttonAddFilter.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.buttonAddFilter.Location = new System.Drawing.Point(3, 309);
+			this.buttonAddFilter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.buttonAddFilter.Name = "buttonAddFilter";
-			this.buttonAddFilter.Size = new System.Drawing.Size(224, 23);
+			this.buttonAddFilter.Size = new System.Drawing.Size(232, 31);
 			this.buttonAddFilter.TabIndex = 1;
 			this.buttonAddFilter.Text = "Add Filter";
 			this.buttonAddFilter.UseVisualStyleBackColor = true;
@@ -306,13 +327,14 @@ namespace cg_proj_1 {
 			// 
 			// MainWindow
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1166, 771);
+			this.ClientSize = new System.Drawing.Size(1333, 1028);
 			this.Controls.Add(this.splitContainer1);
 			this.Controls.Add(this.mainStatusStrip);
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
 			this.Name = "MainWindow";
 			this.Text = "Filter Editor";
 			this.menuStrip1.ResumeLayout(false);
